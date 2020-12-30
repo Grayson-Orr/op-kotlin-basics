0e0d3029849abf1bf9da95da8a5b6fd980fe50a3
Grayson Orr <graysono@op.ac.nz>
2020-12-30 20:10:04 +1300

    Update 04-classes-inheritance.kt

--- 04-classes-inheritance.kt
+++ 04-classes-inheritance.kt
@@ -12,17 +12,17 @@ class Employee(val id: Int, var firstName: String, var lastName: String)
 Inheritance
 ==================================================*/
 
-// In Kotlin, classes & methods are final by default (not allowed to be overridden). If you want to allow class inheritance, mark the class with the open modifier
+// In Kotlin, classes & methods are final by default which means they are not allowed 
+// to be overridden. If you need class inheritance, mark the class with the open modifier
 open class Dog {
   open fun sound() {
     println("bow wow wow!")
   }
 }
 
-// The empty parentheses indicate an invocation of the superclasses default constructor
+// Empty parentheses indicate an invocation of the superclasses default constructor
 class Bulldog : Dog() {
-  // Overriding methods or attributes requires the override modifier.
-  override fun sound() { 
+  override fun sound() { // Overriding methods or attributes require the override modifier
     println("woof woof woof!")
   }
 }
@@ -37,24 +37,26 @@ open class Tiger(val region: String, val sound: String) {
   }
 }
 
-// If you want to use a parameterized constructor of the superclass, provide the constructor args in the subclass declaration
+// If you need to use a parameterised constructor of the superclass, provide the constructor args in the subclass declaration
 class SiberianTiger : Tiger("Sikhote-Alin", "roooaaar")  
 
 /*==================================================
 Passing constructor args to superclass
 ==================================================*/
 
-// region in the BengalTiger declaration is neither a var nor val (it is a constructor arg), whose value is passed to the name prop of the superclass Tiger
+// region in the declaration is neither a var nor val (it is a constructor arg), whose 
+// value is passed to the name prop of the superclass
 class BengalTiger(region: String) : Tiger(region = region, sound = "roooaaar")
 
 fun main() {
-  // Create a new instance of Person via the default constructor. In Kotlin, the new keyword is not used.
+  // New instance of Person via the default constructor. In Kotlin, the new keyword is not used
   val person = Person()
   println(person)
+  
   val employee = Employee(1, "John", "Doe")
-  employee.firstName = "Jane" // Updating the Employee object's firstName to Jane
+  employee.firstName = "Jane" // Updating the object's firstName to Jane
   println(employee)
-  println("${employee.firstName} ${employee.lastName}") // Accessing the Employee object's firstName & lastName props
+  println("${employee.firstName} ${employee.lastName}") // Accessing the object's firstName & lastName props
 
   val dog: Dog = Bulldog()
   dog.sound()
