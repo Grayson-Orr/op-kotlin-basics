9286fb319b89110cf2a4778cc7f0043b76d9b0cd
Grayson Orr <graysono@op.ac.nz>
2021-01-02 21:04:58 +1300

    Update and rename 04-classes-inheritance.kt to 06-class.kt

---
+++ 06-class.kt
@@ -0,0 +1,26 @@
+class Person(
+  val id: Int,
+  var firstName: String,
+  var lastName: String
+) {
+  override fun toString() = "Person(id=$id, firstName=$firstName, lastName=$lastName)"
+}
+
+class Rectangle(val height: Int, val width: Int) {
+  val isSquare: Boolean
+  get() {
+    return height == width
+  }
+}
+
+fun main() {
+  val person = Person(1, "John", "Doe")
+  person.firstName = "Joe"
+  println(person)
+  println(person.firstName)
+
+  val rectangle = Rectangle(10, 20)
+  val square = Rectangle(20, 20)
+  println(rectangle.isSquare)
+  println(square.isSquare)
+}
