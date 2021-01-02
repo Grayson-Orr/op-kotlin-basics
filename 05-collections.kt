7ffc3af5b5431da5784a45f7ac4d47cc1f674414
Grayson Orr <graysono@op.ac.nz>
2021-01-02 20:54:20 +1300

    Update 05-collections.kt

--- 05-collections.kt
+++ 05-collections.kt
@@ -1,3 +1,6 @@
+val progLangs: MutableList<String> = mutableListOf("C#", "Go", "Java")
+val readOnlyProgLangs: List<String> = progLangs
+
 fun addProgLang(progLang: String) {
   progLangs.add(progLang)
 }
@@ -5,6 +8,10 @@ fun addProgLang(progLang: String) {
 fun getProgLanguages(): List<String> {
   return readOnlyProgLangs
 }
+
+const val TRANS_FEE: Int = 15  
+val bankAccounts: MutableMap<String, Int> = mutableMapOf("YouMoney" to 100, "Personal OnCall" to 100, "Rapid Save" to 100)
+val bankReport: Map<String, Int> = bankAccounts
 
 fun addTransactionFee(accountName: String) {
   if (bankAccounts.containsKey(accountName)) {
@@ -22,6 +29,10 @@ fun bankAccountReport() {
   }
 }
 
+val microsoftCEOs: MutableSet<String> = mutableSetOf("Bill Gates", "Steve Ballmer", "Satya Nadella")
+val newCEO: String = "John Doe"
+val existingCEO: String = "Steve Ballmer"
+
 fun addCEO(uniqueCEODesc: String): Boolean {                                                       
   return microsoftCEOs.add(uniqueCEODesc)
 }
@@ -30,30 +41,19 @@ fun getStatusLog(isAdded: Boolean): String {
   return if (isAdded) "was successfully added" else "already exists"
 }
 
-fun main() {
-  val progLangs: MutableList<String> = mutableListOf("C#", "Go", "Java")
-  val readOnlyProgLangs: List<String> = progLangs
- 
+fun main() { 
   addProgLang("MATLAB")
   println("Total programming languages: ${getProgLanguages().size}") // Total programming languages: 4
   getProgLanguages().forEach {
     i -> println(i)
   }
-  
-  const val TRANS_FEE: Int = 15  
-  val bankAccounts: MutableMap<String, Int> = mutableMapOf("YouMoney" to 100, "Personal OnCall" to 100, "Rapid Save" to 100)
-  val bankReport: Map<String, Int> = bankAccounts
  
   bankAccountReport()
   addTransactionFee("Rapid Save") 
   addTransactionFee("Rapid Save") 
   addTransactionFee("Term Deposit")
   bankAccountReport()
-  
-  val microsoftCEOs: MutableSet<String> = mutableSetOf("Bill Gates", "Steve Ballmer", "Satya Nadella")
-  val newCEO: String = "John Doe"
-  val existingCEO: String = "Steve Ballmer"
-  
+    
   println("$newCEO ${getStatusLog(addCEO(newCEO))}")
   println("$existingCEO ${getStatusLog(addCEO(existingCEO))}")
 }
